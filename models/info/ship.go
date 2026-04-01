72123e49a0426f9aa18add23db3eaa595e9bfb23
Daniel Vogt <daniel.vogt@blackforestbytes.de>
2026-04-01 11:58:16 +0200

    Fix cannons

--- models/info/ship.go
+++ models/info/ship.go
@@ -4,7 +4,7 @@ type InfoShip struct {
 	Id      string `json:"id"`
 	Name    string `json:"name"`
 	Sight   int    `json:"sight"`
-	Canons  int    `json:"canons"`
+	Canons  int    `json:"cannons"`
 	Tdwat   int    `json:"-"`
 	Draught int    `json:"-"`
 	Speed   int    `json:"speed"`
